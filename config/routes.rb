7c53d709cc13e9f30a7f8030fe514a3fddff2e72
Srija S <srijakumar7@gmail.com>
2019-06-14 14:46:33 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/no-test-framework'
+  get 'secrets/new'
 
   root 'application#hello'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post 'logout' => 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
