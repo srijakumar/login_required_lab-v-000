5a002dfc1e7a3bcee7e08ba76e0b899d97958120
Srija S <srijakumar7@gmail.com>
2019-06-14 14:30:25 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'sessions/no-test-framework'
 
+  root 'application#hello'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
