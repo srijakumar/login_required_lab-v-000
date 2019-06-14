c641381d2a0a16ef250af4e796ffd49d29611f20
Srija S <srijakumar7@gmail.com>
2019-06-14 14:47:11 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   post 'logout' => 'sessions#destroy'
 
+  get '/secret' => 'secrets#show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
